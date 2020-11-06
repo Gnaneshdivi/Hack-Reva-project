2d4b7c5d465a95ce3c3d455e7568de38101ed1c1
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2020-11-07 01:22:01 +0530

    Model files updated

--- Model Files/Importing_model.py
+++ Model Files/Importing_model.py
@@ -6,7 +6,7 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = tensorflow.keras.models.load_model('keras_model.h5')
+model = tensorflow.keras.models.load_model('/keras_model.h5')
 
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
@@ -14,7 +14,7 @@ model = tensorflow.keras.models.load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open('test_photo.jpg')
+image = Image.open('/test_tulsi.jpg')
 
 #resize the image to a 224x224 with the same strategy as in TM2:
 #resizing the image to be at least 224x224 and then cropping from the center
@@ -35,4 +35,14 @@ data[0] = normalized_image_array
 
 # run the inference
 prediction = model.predict(data)
-print(prediction)
+
+# 0 Mango
+# 1 Guava
+# 2 Tulsi
+# 3 Neem
+
+classes = ['Mango','Guava','Tulsi','Neem']
+
+
+
+print ("{}".format(classes[prediction.argmax()]) if prediction.max()>0.5 else "Error")
---
+++ Model Files/Model_Descripton.md
@@ -0,0 +1,30 @@
+# Model Description
+
+The dataset is divided into 85% of training set and 15% of testing set.
+
+The accuracy per class found on testing is :
+
+
+
+
+| Class         | Accuracy        | #Samples   |
+| :------------ |:---------------:| -----:     |
+| Mango         | 0.37              |  19      |
+| Guava         | 0.64              |  44      |
+| Tulsi         | 0.79              |  100     |
+| Neem          | 0.81              |  63      |
+
+
+Confusion matrix :
+
+![Optional Text](..\Model Files\Confusion matrix.png)
+
+
+## Further details :
+
+No.Epochs = 50
+Bacth size = 16
+
+Learning rate = 0.001
+
+
